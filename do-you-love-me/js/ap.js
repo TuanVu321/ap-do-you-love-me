d92058f0862cf1dcfb8207ec80f8dbd629835bb1
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-02-23 14:14:08 +0700

    Add files via upload

---
+++ do-you-love-me/js/ap.js
@@ -0,0 +1,4 @@
+function setrandom() {
+    document.getElementById("no").style.top = Math.floor(Math.random() * 200) - "px";
+    document.getElementById("no").style.left = Math.floor(Math.random() * 400) - "px";
+}
